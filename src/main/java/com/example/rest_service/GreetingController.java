226aa2bf72708abe261ea8aaf641fde96191c545
Kevin <kcrapo@csumb.edu>
2025-02-25 13:13:01 -0800

    Initial Commit
    - Boilerplate rest-service Springboot template.
    
    - Has rest controller named Greeting Controller with an example of RequestParams and GetMappings.
    
    - Barebones dependencies and gitignore

---
+++ src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.rest_service;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**  curl "http://localhost:8080/greeting?name=User"
+    // greeting is the route
+     ?name = is the request param
+     **/
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
