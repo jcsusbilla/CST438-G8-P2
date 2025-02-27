8e55fa53ec07655bb45ac5ed029e590057d6c81e
Kevin <kcrapo@csumb.edu>
2025-02-26 19:52:05 -0800

    Refactoring and some notes

--- src/main/java/com/example/rest_service/database/DatabaseRepository.java
+++ src/main/java/com/example/rest_service/database/DatabaseRepository.java
@@ -1,13 +1,13 @@
 package com.example.rest_service.database;
 import com.example.rest_service.database.entities.User;
-import com.example.rest_service.database.entities.TierList;
-import com.example.rest_service.database.entities.TierList;
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 // This allows us to initialize an instance of our database
-public interface databaseRepository extends CrudRepository<User, Integer> {
+// Should I create a repository for each entity or store them all in one?
+// And I was reading up on how to do CRUD operations in spring and need to look into Services
+public interface DatabaseRepository extends CrudRepository<User, Integer> {
 
 }
