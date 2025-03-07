1c012444a7293d0991164ea1a9c53ad68cc2eca0
DanielBernal7 <danbernal@csumb.edu>
2025-03-07 03:29:42 -0800

    Changed the /user/add route to have first_name and last_name paramaters to be optional
    Change the requestMapping in the TierRankingController.java from "/tierranking" to "/tier-ranking".
    
    Added /tier-rankings/add route tothe TierRankingController.java.

--- src/main/java/com/example/rest_service/database/controllers/TierRankingController.java
+++ src/main/java/com/example/rest_service/database/controllers/TierRankingController.java
@@ -1,16 +1,45 @@
 package com.example.rest_service.database.controllers;
 
 
+import com.example.rest_service.database.entities.TierList;
 import com.example.rest_service.database.entities.TierRanking;
 import com.example.rest_service.database.repositories.TierRankingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/tierrankings")
+@RequestMapping("/tier-rankings")
 public class TierRankingController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addRanking(@RequestParam int tierListId,
+                                             @RequestParam String item,
+                                             @RequestParam String tier){
+
+        //This is to check isf the tier list exists
+        String checkTierListSQL = "SELECT COUNT(*) FROM tier_list WHERE id = ?";
+        Integer count = jdbcTemplate.queryForObject(checkTierListSQL, Integer.class, tierListId);
+
+        if(count == null || count == 0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tier List not found.");
+        }
+
+        //This inserts the values into the tier list
+        String insertSQL = "INSERT INTO tier_ranking (tier_list_id, item, tier) VALUES (?, ?, ?)";
+        jdbcTemplate.update(insertSQL, tierListId, item, tier);
+
+        return ResponseEntity.ok("Ranking added successfully.");
+    }
+
+
 }
